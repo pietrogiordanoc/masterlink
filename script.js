d9c8e4548d93c2da2886502fac3e4b91d74d8c0b
pietrogiordanoc <36571527+pietrogiordanoc@users.noreply.github.com>
2025-04-24 13:46:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}const ANIMATION_DURATION = 300;
+const ANIMATION_DURATION = 300;
 
 const SIDEBAR_EL = document.getElementById("sidebar");
 
 const SUB_MENU_ELS = document.querySelectorAll(
-".menu > ul > .menu-item.sub-menu");
-
+  ".menu > ul > .menu-item.sub-menu"
+);
 
 const FIRST_SUB_MENUS_BTN = document.querySelectorAll(
-".menu > ul > .menu-item.sub-menu > a");
-
+  ".menu > ul > .menu-item.sub-menu > a"
+);
 
 const INNER_SUB_MENUS_BTN = document.querySelectorAll(
-".menu > ul > .menu-item.sub-menu .menu-item.sub-menu > a");
-
+  ".menu > ul > .menu-item.sub-menu .menu-item.sub-menu > a"
+);
 
 class PopperObject {
+  instance = null;
+  reference = null;
+  popperTarget = null;
 
-
-
-
-  constructor(reference, popperTarget) {_defineProperty(this, "instance", null);_defineProperty(this, "reference", null);_defineProperty(this, "popperTarget", null);
+  constructor(reference, popperTarget) {
     this.init(reference, popperTarget);
   }
 
@@ -31,26 +31,26 @@ class PopperObject {
       strategy: "fixed",
       resize: true,
       modifiers: [
-      {
-        name: "computeStyles",
-        options: {
-          adaptive: false } },
-
-
-      {
-        name: "flip",
-        options: {
-          fallbackPlacements: ["left", "right"] } }] });
-
-
-
-
+        {
+          name: "computeStyles",
+          options: {
+            adaptive: false
+          }
+        },
+        {
+          name: "flip",
+          options: {
+            fallbackPlacements: ["left", "right"]
+          }
+        }
+      ]
+    });
 
     document.addEventListener(
-    "click",
-    e => this.clicker(e, this.popperTarget, this.reference),
-    false);
-
+      "click",
+      (e) => this.clicker(e, this.popperTarget, this.reference),
+      false
+    );
 
     const ro = new ResizeObserver(() => {
       this.instance.update();
@@ -62,54 +62,54 @@ class PopperObject {
 
   clicker(event, popperTarget, reference) {
     if (
-    SIDEBAR_EL.classList.contains("collapsed") &&
-    !popperTarget.contains(event.target) &&
-    !reference.contains(event.target))
-    {
+      SIDEBAR_EL.classList.contains("collapsed") &&
+      !popperTarget.contains(event.target) &&
+      !reference.contains(event.target)
+    ) {
       this.hide();
     }
   }
 
   hide() {
     this.instance.state.elements.popper.style.visibility = "hidden";
-  }}
-
+  }
+}
 
 class Poppers {
+  subMenuPoppers = [];
 
-
-  constructor() {_defineProperty(this, "subMenuPoppers", []);
+  constructor() {
     this.init();
   }
 
   init() {
-    SUB_MENU_ELS.forEach(element => {
+    SUB_MENU_ELS.forEach((element) => {
       this.subMenuPoppers.push(
-      new PopperObject(element, element.lastElementChild));
-
+        new PopperObject(element, element.lastElementChild)
+      );
       this.closePoppers();
     });
   }
 
   togglePopper(target) {
     if (window.getComputedStyle(target).visibility === "hidden")
-    target.style.visibility = "visible";else
-    target.style.visibility = "hidden";
+      target.style.visibility = "visible";
+    else target.style.visibility = "hidden";
   }
 
   updatePoppers() {
-    this.subMenuPoppers.forEach(element => {
+    this.subMenuPoppers.forEach((element) => {
       element.instance.state.elements.popper.style.display = "none";
       element.instance.update();
     });
   }
 
   closePoppers() {
-    this.subMenuPoppers.forEach(element => {
+    this.subMenuPoppers.forEach((element) => {
       element.hide();
     });
-  }}
-
+  }
+}
 
 const slideUp = (target, duration = ANIMATION_DURATION) => {
   const { parentElement } = target;
@@ -170,7 +170,7 @@ const slideDown = (target, duration = ANIMATION_DURATION) => {
 
 const slideToggle = (target, duration = ANIMATION_DURATION) => {
   if (window.getComputedStyle(target).display === "none")
-  return slideDown(target, duration);
+    return slideDown(target, duration);
   return slideUp(target, duration);
 };
 
@@ -192,9 +192,9 @@ document.getElementById("btn-collapse").addEventListener("click", () => {
   SIDEBAR_EL.classList.toggle("collapsed");
   PoppersInstance.closePoppers();
   if (SIDEBAR_EL.classList.contains("collapsed"))
-  FIRST_SUB_MENUS_BTN.forEach(element => {
-    element.parentElement.classList.remove("open");
-  });
+    FIRST_SUB_MENUS_BTN.forEach((element) => {
+      element.parentElement.classList.remove("open");
+    });
 
   updatePoppersTimeout();
 });
@@ -217,27 +217,27 @@ document.getElementById("overlay").addEventListener("click", () => {
 
 const defaultOpenMenus = document.querySelectorAll(".menu-item.sub-menu.open");
 
-defaultOpenMenus.forEach(element => {
+defaultOpenMenus.forEach((element) => {
   element.lastElementChild.style.display = "block";
 });
 
 /**
  * handle top level submenu click
  */
-FIRST_SUB_MENUS_BTN.forEach(element => {
+FIRST_SUB_MENUS_BTN.forEach((element) => {
   element.addEventListener("click", () => {
     if (SIDEBAR_EL.classList.contains("collapsed"))
-    PoppersInstance.togglePopper(element.nextElementSibling);else
-    {
+      PoppersInstance.togglePopper(element.nextElementSibling);
+    else {
       const parentMenu = element.closest(".menu.open-current-submenu");
       if (parentMenu)
-      parentMenu.
-      querySelectorAll(":scope > ul > .menu-item.sub-menu > a").
-      forEach(
-      (el) =>
-      window.getComputedStyle(el.nextElementSibling).display !==
-      "none" && slideUp(el.nextElementSibling));
-
+        parentMenu
+          .querySelectorAll(":scope > ul > .menu-item.sub-menu > a")
+          .forEach(
+            (el) =>
+              window.getComputedStyle(el.nextElementSibling).display !==
+                "none" && slideUp(el.nextElementSibling)
+          );
       slideToggle(element.nextElementSibling);
     }
   });
@@ -246,7 +246,7 @@ FIRST_SUB_MENUS_BTN.forEach(element => {
 /**
  * handle inner submenu click
  */
-INNER_SUB_MENUS_BTN.forEach(element => {
+INNER_SUB_MENUS_BTN.forEach((element) => {
   element.addEventListener("click", () => {
     slideToggle(element.nextElementSibling);
   });
