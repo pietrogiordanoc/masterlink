5b8b86e5f8980a8c1bcdc9889d9924fe8d56f5d6
pietrogiordanoc <36571527+pietrogiordanoc@users.noreply.github.com>
2025-04-24 13:38:39 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-var input = document.querySelector('.menu');
-	var button = document.querySelector('button');	
-		button.addEventListener('click', function(e){
-			e.preventDefault();
-			input.classList.toggle('active');
-		})
